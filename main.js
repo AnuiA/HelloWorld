262666f0d923af510eb4af76616ea79737ca93f7
LENOVO-PC\lenovo <837405641@qq.com>
2016-09-18 22:01:24 +0800

    金字塔作业_半成品（未实现输入功能）

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log("HelloWorld!");
+console.log("   HelloWorld!");
+function printT(T) {
+    var i;
+    for (i = 1; i <= T; i++) {
+        draw(i, T);
+        //        console.log(k);
+        console.log(S);
+        S = "";
+    }
+}
+function draw(Ceng, Total) {
+    var i, j;
+    //    console.log(k);
+    for (i = 0; i < Total - Ceng; i++) {
+        drawB();
+    }
+    for (j = 0; j < 2 * Ceng - 1; j++) {
+        drawX();
+    }
+}
+function drawX() {
+    S = S + "*";
+}
+function drawB() {
+    S = " " + S;
+}
+var S;
+S = "";
+console.log("   " + S);
+var k;
+k = 10;
+printT(k);
--- main.ts
+++ main.ts
@@ -1,3 +1,41 @@
 
 
-console.log("HelloWorld!");
+console.log("   HelloWorld!");
+
+function printT ( T:number ):void {
+    var i:number;    
+    for(i=1;i<=T;i++) {
+        draw(i,T);
+//        console.log(k);
+        console.log(S);
+        S = "";
+    }
+}
+
+function draw (Ceng:number,Total:number):void {
+    var i:number,j:number;
+//    console.log(k);
+    for(i=0;i<Total-Ceng;i++) {
+        drawB();
+    } 
+    for(j=0;j<2*Ceng-1;j++) {
+        drawX();
+    }
+}
+
+function drawX ( ):void { 
+    S = S + "*";
+}
+
+function drawB ( ):void {
+    S = " " + S;
+}
+
+var S:string;
+S = "";
+console.log("   "+S);
+
+var k:number;
+k = 10;
+printT(k);
+
